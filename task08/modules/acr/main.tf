56f83ed9ccf2c43f7768795c05dfe041c23f0cb0
nitinkushwaha <nitinkushwaha706@outlook.com>
2026-04-25 22:41:58 +0530

    fix bug

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_container_registry_task" "ACR-task" {
   }
 
   docker_step {
-    dockerfile_path      = "Dockerfile"
+    dockerfile_path      = "application/Dockerfile"
     context_path         = var.repo_url
     context_access_token = var.git_pat
     image_names          = ["${var.image_name}:latest"]
